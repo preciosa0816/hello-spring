1f56b8b30dec8aa1b055dca0315cb8f1bfaba7f0
Yunji Park <preciosa0816@gmail.com>
2024-06-04 00:58:06 +0900

    0604 섹션 3 회원도메인과 리포지토리 만들기

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -22,7 +22,7 @@ public class MemoryMemberRepository implements MemberRepository
     }
 
     @Override
-    public Optional<Member> findByName(String email) {
+    public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name)).findAny();
     }
