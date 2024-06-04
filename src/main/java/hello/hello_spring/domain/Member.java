724b7c6b4e68846c3e03db00ca5a09028870089c
Yunji Park <preciosa0816@gmail.com>
2024-06-05 00:07:54 +0900

    0604 섹션 3 회원도메인과 리포지토리 만들기( test case 만들기)

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -3,20 +3,16 @@ package hello.hello_spring.domain;
 public class Member {
     private Long id;
     private String name;
-
     public Long getId() {
         return id;
     }
-
-    public String getName() {
-        return name;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+    public String getName() {
+        return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-
 }
